480da31630e387ffa6f05f1023e5f42db361fcc6
fabian0702 <48295869+fabian0702@users.noreply.github.com>
2025-05-22 21:25:21 +0200

    added proxy manager

---
+++ proxy/manager/manager.go
@@ -0,0 +1,65 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/fabian0702/verona/proxy/proxy"
+)
+
+type Manager struct {
+	proxies map[string]*proxy.Proxy
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		proxies: make(map[string]*proxy.Proxy),
+	}
+}
+
+func (m *Manager) StartProxy(id string, localPort int, remoteHost string, remotePort int) error {
+	p := proxy.NewProxy(localPort, remoteHost, remotePort)
+	if err := p.Start(); err != nil {
+		return err
+	}
+	m.proxies[id] = p
+	return nil
+}
+
+func (m *Manager) StopProxy(id string) error {
+	if p, ok := m.proxies[id]; ok {
+		p.Stop()
+		delete(m.proxies, id)
+		return nil
+	}
+	return fmt.Errorf("proxy with id %s not found", id)
+}
+
+func (m *Manager) ChangeDestination(id string, remoteHost string, remotePort int) error {
+	if p, ok := m.proxies[id]; ok {
+		p.ChangeDestination(remoteHost, remotePort)
+		return nil
+	}
+	return fmt.Errorf("proxy with id %s not found", id)
+}
+
+func (m *Manager) StopAll() {
+	fmt.Printf("Stopping all proxies\n")
+	for id, p := range m.proxies {
+		p.Stop()
+		fmt.Printf("Stopping proxy %s\n", id)
+		delete(m.proxies, id)
+	}
+}
+
+func (m *Manager) UnPauseProxy(id string, paused bool) error {
+	if p, ok := m.proxies[id]; ok {
+		p.SetCacheEnabled(paused)
+		status := "resumed"
+		if paused {
+			status = "paused"
+		}
+		fmt.Printf("Proxy %s %s\n", id, status)
+		return nil
+	}
+	return fmt.Errorf("proxy with id %s not found", id)
+}
