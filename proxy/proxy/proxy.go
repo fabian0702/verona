41a870c483ee070d84d171b9c956cc1baa14e6f4
fabian0702 <48295869+fabian0702@users.noreply.github.com>
2025-05-22 21:25:06 +0200

    minor changes

--- proxy/proxy/proxy.go
+++ proxy/proxy/proxy.go
@@ -1,4 +1,4 @@
-package main
+package proxy
 
 import (
 	"fmt"
@@ -9,18 +9,29 @@ import (
 )
 
 type Proxy struct {
-	local    string
-	remote   string
-	useCache bool
-	cacheWg  sync.WaitGroup
+	local     string
+	remote    string
+	UseCache  bool
+	cacheWg   sync.WaitGroup
+	terminate chan bool
 }
 
 func NewProxy(localPort int, remoteHost string, remotePort int) *Proxy {
 	return &Proxy{
-		local:   fmt.Sprintf("0.0.0.0:%d", localPort),
-		remote:  fmt.Sprintf("%s:%d", remoteHost, remotePort),
-		cacheWg: sync.WaitGroup{},
+		local:     fmt.Sprintf("0.0.0.0:%d", localPort),
+		remote:    fmt.Sprintf("%s:%d", remoteHost, remotePort),
+		cacheWg:   sync.WaitGroup{},
+		terminate: make(chan bool),
 	}
+}
+
+func (p *Proxy) ChangeDestination(remoteHost string, remotePort int) {
+	p.remote = fmt.Sprintf("%s:%d", remoteHost, remotePort)
+	log.Printf("Changed destination to %s", p.remote)
+}
+func (p *Proxy) Stop() {
+	p.terminate <- true
+	log.Println("Proxy stopped")
 }
 
 // Start starts the proxy server
@@ -35,22 +46,28 @@ func (p *Proxy) Start() error {
 	log.Printf("Proxy listening on %s, forwarding to %s", p.local, p.remote)
 
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
-			continue
-		}
+		select {
+		case <-p.terminate:
+			log.Println("Stopping proxy server")
+			return nil
+		default:
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Printf("Failed to accept connection: %v", err)
+				continue
+			}
 
-		go p.handleConnection(conn)
+			go p.handleConnection(conn)
+		}
 	}
 }
 
 func (p *Proxy) SetCacheEnabled(enabled bool) {
-	if enabled && p.useCache {
-		p.useCache = true
+	if enabled && p.UseCache {
+		p.UseCache = true
 		p.cacheWg.Add(1)
 	} else {
-		p.useCache = false
+		p.UseCache = false
 		p.cacheWg.Done()
 	}
 }
